086903a2f9cc2b7d12c3e7c76eaa3a901daef944
luciadmaddalena <dellam.lucia@outlook.com>
2025-09-15 21:16:57 -0300

    creacion de funciones en javascript, termine el css, y cambios en html

---
+++ js/app.js
@@ -0,0 +1,42 @@
+//Toogle para modo oscuro/claro
+const themeToggle = document.getElementById('themeToggle');
+const iconTheme = themeToggle.querySelector('i');
+
+//verificar preferencia del usuario
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-mode');
+    iconTheme.classList.remove('fa-moon');
+    iconTheme.classList.add('fa-sun');
+}
+
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+        iconTheme.classList.remove('fa-moon');
+        iconTheme.classList.add('fa-sun');
+    } else {
+        localStorage.setItem('theme', 'light');
+        iconTheme.classList.remove('fa-sun');
+        iconTheme.classList.add('fa/moon');
+    }
+})
+
+//smooth scroll para navegacion
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                
+                const targetId = this.getAttribute('href');
+                if (targetId === '#') return;
+                
+                const targetElement = document.querySelector(targetId);
+                if (targetElement) {
+                    window.scrollTo({
+                        top: targetElement.offsetTop - 80,
+                        behavior: 'smooth'
+                    });
+                }
+            });
+        });
